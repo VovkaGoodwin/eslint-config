77e9675da185e054eca205060c82f2d464198407
Vovka Goodwin <volodya.radko@gmail.com>
2024-08-26 22:09:42 +0700

    миграция на eslint v9

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
-    extends: [
-        './rules/react.cjs',
-        './rules/general.cjs'
-    ].map(require.resolve),
+    mjs: {
+        general: require('./rules/general')
+    }
 }
